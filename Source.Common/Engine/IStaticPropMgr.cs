80a24cfa9a8152077e3905c02a709ddd7d099bd4
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-08-11 15:17:15 +0200

    Add StaticPropMgr interfaces for the future

---
+++ Source.Common/Engine/IStaticPropMgr.cs
@@ -0,0 +1,43 @@
+﻿namespace Source.Common.Engine;
+using System.Collections.Generic;
+using System.Numerics;
+
+public class Ray_t { }
+public class trace_t { }
+public class IClientRenderable { }
+public class IPhysicsEnvironment { }
+public class IVPhysicsKeyHandler { }
+public class ICollideable { }
+public struct CBaseHandle { }
+public class IHandleEntity { }
+
+public interface IStaticPropMgr
+{
+	void CreateVPhysicsRepresentations(IPhysicsEnvironment physenv, IVPhysicsKeyHandler defaults, object gameData);
+
+	void TraceRayAgainstStaticProp(Ray_t ray, int staticPropIndex, trace_t tr);
+
+	bool IsStaticProp(IHandleEntity handleEntity);
+	bool IsStaticProp(CBaseHandle handle);
+	ICollideable GetStaticPropByIndex(int propIndex);
+}
+
+public interface IStaticPropMgrClient : IStaticPropMgr
+{
+	void ComputePropOpacity(Vector3 viewOrigin, float factor);
+	void AddDecalToStaticProp(Vector3 rayStart, Vector3 rayEnd, int staticPropIndex, int decalIndex, bool doTrace, trace_t tr);
+	void AddShadowToStaticProp(ushort shadowHandle, IClientRenderable renderable);
+	void RemoveAllShadowsFromStaticProp(IClientRenderable renderable);
+	void GetStaticPropMaterialColorAndLighting(trace_t trace, int staticPropIndex, out Vector3 lighting, out Vector3 matColor);
+	void GetAllStaticProps(List<ICollideable> output);
+	void GetAllStaticPropsInAABB(Vector3 mins, Vector3 maxs, List<ICollideable> output);
+	void GetAllStaticPropsInOBB(Vector3 origin, Vector3 extent1, Vector3 extent2, Vector3 extent3, List<ICollideable> output);
+	void DrawStaticProps(IClientRenderable[] props, int count, bool shadowDepth, bool drawVCollideWireframe);
+}
+
+public interface IStaticPropMgrServer : IStaticPropMgr
+{
+	void GetAllStaticProps(List<ICollideable> output);
+	void GetAllStaticPropsInAABB(Vector3 mins, Vector3 maxs, List<ICollideable> output);
+	void GetAllStaticPropsInOBB(Vector3 origin, Vector3 extent1, Vector3 extent2, Vector3 extent3, List<ICollideable> output);
+}
