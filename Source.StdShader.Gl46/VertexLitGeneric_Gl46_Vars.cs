5a97b6d88c15900d7bbf2e176a43f3615613fe17
march <106459595+marchc1@users.noreply.github.com>
2025-08-12 17:27:18 -0700

    Create VertexLitGeneric_Gl46_Vars.cs

---
+++ Source.StdShader.Gl46/VertexLitGeneric_Gl46_Vars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.StdShader.Gl46;
+
+public struct VertexLitGeneric_Gl46_Vars
+{
+	public int BaseTexture;
+}
