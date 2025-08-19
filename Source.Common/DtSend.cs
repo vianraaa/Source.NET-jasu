18e996cebbc152d64b7564c69492ac337d81e3a8
march <106459595+marchc1@users.noreply.github.com>
2025-08-18 22:53:18 -0700

    Further scaffolding

---
+++ Source.Common/DtSend.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Common;
+
+public class SendProp {
+
+}
+
+public class SendTable
+{
+
+}
---
+++ Source.Common/ServerClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Common;
+
+public class ServerClass
+{
+	public string NetworkName;
+	public SendTable Table;
+	public ServerClass? Next;
+	public int ClassID;
+	public int InstanceBaselineIndex;
+	public ServerClass(ReadOnlySpan<char> networkName, SendTable table) {
+
+	}
+}
