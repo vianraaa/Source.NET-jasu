371f685422a9440295160de5dbe6876be0f24a53
march <106459595+marchc1@users.noreply.github.com>
2025-08-28 20:10:51 -0700

    Further IScheme impl

--- Source.GUI/Scheme.cs
+++ Source.GUI/Scheme.cs
@@ -1,7 +1,11 @@
-﻿using Source.Common.Engine;
+﻿using Source.Common.Client;
+using Source.Common.Engine;
 using Source.Common.Formats.Keyvalues;
 using Source.Common.GUI;
 using Source.Common.Launcher;
+
+using System;
+using System.Globalization;
 
 namespace Source.GUI;
 
@@ -72,17 +76,65 @@ public class Scheme : IScheme
 	private void LoadFonts() {
 		Span<char> language = stackalloc char[64];
 		bool valid = System.GetRegistryString("HKEY_CURRENT_USER\\Software\\Valve\\Source\\Language", language);
-		if (!valid) 
+		if (!valid)
 			"english".CopyTo(language);
-		
-		for(var kv = Data.FindKey("CustomFontFiles", true)!.GetFirstSubKey(); kv != null; kv = kv.GetNextKey()) {
+
+		for (var kv = Data.FindKey("CustomFontFiles", true)!.GetFirstSubKey(); kv != null; kv = kv.GetNextKey()) {
 			ReadOnlySpan<char> fontFile = kv.GetString();
-			if (fontFile != null && fontFile.Length > 0) {
+			if (fontFile != null && fontFile[0] != 0) {
 				Surface.AddCustomFontFile(null, fontFile);
 			}
 			else {
+				int rangeMin = 0, rangeMax = 0;
+				ReadOnlySpan<char> name = null;
+				bool useRange = false;
 
+				for (KeyValues? data = kv.GetFirstSubKey(); data != null; data = data.GetNextKey()) {
+					ReadOnlySpan<char> key = data.Name;
+					if (key.Equals("font", StringComparison.OrdinalIgnoreCase))
+						fontFile = data.GetString();
+					else if (key.Equals("name", StringComparison.OrdinalIgnoreCase))
+						name = data.GetString();
+					else {
+						if (key.Equals(language, StringComparison.OrdinalIgnoreCase)) {
+							KeyValues? pRange = data.FindKey("range");
+							if (pRange != null) {
+								useRange = true;
+								string[] parts = pRange.GetString().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+								if (
+									parts.Length >= 2 &&
+									int.TryParse(parts[0], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out rangeMin) &&
+									int.TryParse(parts[1], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out rangeMax)
+								)
+									if (rangeMin > rangeMax)
+										(rangeMax, rangeMin) = (rangeMin, rangeMax);
+							}
+						}
+					}
+
+					if (fontFile != null && fontFile[0] != 0) {
+						Surface.AddCustomFontFile(name, fontFile);
+
+						if (useRange)
+							SetFontRange(name, rangeMin, rangeMax);
+					}
+				}
 			}
 		}
+	}
+
+	record struct FontRange {
+		public int Min;
+		public int Max;
+	}
+
+	Dictionary<ulong, FontRange> FontRanges = [];
+
+	private void SetFontRange(ReadOnlySpan<char> name, int rangeMin, int rangeMax) {
+		ulong symbol = name.Hash();
+		FontRanges[symbol] = new() {
+			Min = rangeMin,
+			Max = rangeMax
+		};
 	}
 }
