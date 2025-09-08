fd11a5d472eac63515b0d47b2f31100982c3fe96
march <106459595+marchc1@users.noreply.github.com>
2025-09-08 15:23:29 -0700

    ROS<char>-ify usermessages

--- Game.Shared/UserMessages.cs
+++ Game.Shared/UserMessages.cs
@@ -3,22 +3,22 @@
 using Source.Common.Bitbuffers;
 
 using Source;
+using Source.Common.Utilities;
 
 public delegate void UserMessageHook(bf_read msg);
 
 public class UserMessage
 {
 	public int Size = 0;
-	public string Name = "";
+	public UtlSymbol Name = new();
 	public List<UserMessageHook> ClientHooks = [];
 }
 
 [EngineComponent]
 public partial class UserMessages
 {
-	private Dictionary<string, int> nameToIntType = [];
+	private Dictionary<UtlSymId_t, int> nameToIntType = [];
 	private Dictionary<int, UserMessage> intTypeToInstance = [];
-
 
 	public int Count {
 		get {
@@ -28,53 +28,51 @@ public partial class UserMessages
 		}
 	}
 
-	public int LookupUserMessage(string name) {
-		if (nameToIntType.TryGetValue(name, out int idx))
+	public int LookupUserMessage(ReadOnlySpan<char> name) {
+		if (nameToIntType.TryGetValue(new UtlSymbol(name), out int idx))
 			return idx;
 		return -1;
 	}
 
 	public int GetUserMessageSize(int index) {
-		if (index < 0 || index >= Count) {
+		if (index < 0 || index >= Count) 
 			throw new Exception($"UserMessages.GetUserMessageSize({index}) out of range!!!");
-		}
 
 		UserMessage e = intTypeToInstance[index];
 		return e.Size;
 	}
 
-	public string GetUserMessageName(int index) {
-		if (index < 0 || index >= Count) {
+	public ReadOnlySpan<char> GetUserMessageName(int index) {
+		if (index < 0 || index >= Count) 
 			throw new Exception($"UserMessages.GetUserMessageSize({index}) out of range!!!");
-		}
 
 		UserMessage e = intTypeToInstance[index];
-		return e.Name;
+		return e.Name.String();
 	}
 
 	public bool IsValidIndex(int index) {
 		return intTypeToInstance.ContainsKey(index);
 	}
 
-	public void Register(string name, int size) {
-		if (nameToIntType.TryGetValue(name, out int idx))
+	public void Register(ReadOnlySpan<char> name, int size) {
+		if (nameToIntType.TryGetValue(new UtlSymbol(name), out int idx))
 			throw new Exception($"UserMessages.Register '{name}' already registered");
 
 		UserMessage entry = new UserMessage();
 		entry.Size = size;
-		entry.Name = name;
+		entry.Name = new UtlSymbol(name);
 		idx = Count;
 
 		intTypeToInstance[idx] = entry;
-		nameToIntType[name] = idx;
+		nameToIntType[entry.Name] = idx;
 	}
 
-	public void HookMessage(string name, UserMessageHook hook) {
-		Dbg.Assert(name != null);
-		Dbg.Assert(hook != null);
+	public void HookMessage(ReadOnlySpan<char> name, UserMessageHook hook) {
+		Assert(name != null);
+		Assert(hook != null);
 
-		if (!nameToIntType.TryGetValue(name, out int idx)) {
-			Dbg.Assert(false, $"UserMessages.HookMessage: no such message {name}");
+		if (!nameToIntType.TryGetValue(new UtlSymbol(name), out int idx)) {
+			Assert(false, $"UserMessages.HookMessage: no such message {name}");
 			return;
 		}
 
@@ -84,17 +82,17 @@ public partial class UserMessages
 
 	public bool DispatchUserMessage(int msgType, bf_read msgData) {
 		if (msgType < 0 || msgType >= Count) {
-			Dbg.Assert(false, $"UserMessages.DispatchUserMessage: Bogus msg type {msgType} (max == {Count})");
+			Assert(false, $"UserMessages.DispatchUserMessage: Bogus msg type {msgType} (max == {Count})");
 			return false;
 		}
 
 		if (!intTypeToInstance.TryGetValue(msgType, out UserMessage? entry)) {
-			Dbg.Assert(false, $"UserMessages.DispatchUserMessage: Missing client entry for msg type {msgType}");
+			Assert(false, $"UserMessages.DispatchUserMessage: Missing client entry for msg type {msgType}");
 			return false;
 		}
 
 		if (entry.ClientHooks.Count == 0) {
-			Dbg.Warning($"UserMessages.DispatchUserMessage: missing client hook for {entry.Name}\n");
+			Warning($"UserMessages.DispatchUserMessage: missing client hook for {entry.Name}\n");
 			return false;
 		}
 
