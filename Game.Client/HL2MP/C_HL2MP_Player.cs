19281e9543d65560038418ea2992e8fd5a57a140
march <106459595+marchc1@users.noreply.github.com>
2025-09-13 13:22:03 -0700

    Add some attributes to get started

--- Game.Client/HL2MP/C_HL2MP_Player.cs
+++ Game.Client/HL2MP/C_HL2MP_Player.cs
@@ -1,9 +1,12 @@
 ﻿using Game.Client.HL2;
+using Game.Shared;
 
 using Source.Common;
 
 namespace Game.Client.HL2MP;
 
+[LinkEntityToClass(LocalName = "player")]
+[ManualClassIndex(Index = 76)]
 public partial class C_HL2MP_Player : C_BaseHLPlayer
 {
 	public override void PostDataUpdate(DataUpdateType updateType) {
---
+++ Game.Shared/PredictableEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Shared;
+
+/// <summary>
+/// Links a class type to a hammer name
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class LinkEntityToClassAttribute : Attribute
+{
+	public required string LocalName;
+}
+
+/// <summary>
+/// Manually sets the class index. This is required at the moment for Garry's Mod networking compat
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class ManualClassIndexAttribute : Attribute
+{
+	public required int Index;
+}
