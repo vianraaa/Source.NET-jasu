c1c1db52840a946d09e8ab6386aab45f372c3cca
march <106459595+marchc1@users.noreply.github.com>
2025-09-15 13:00:53 -0700

    Update BaseEntity.cs

--- Game.Server/BaseEntity.cs
+++ Game.Server/BaseEntity.cs
@@ -18,7 +18,8 @@ public partial class BaseEntity : IServerEntity
 	]);
 
 	public static SendTable DT_BaseEntity = new([
-		new SendPropDataTable<BaseEntity>("AnimTimeMustBeFirst", DT_AnimTimeMustBeFirst)
+		new SendPropDataTable<BaseEntity>("AnimTimeMustBeFirst", DT_AnimTimeMustBeFirst),
+
 	]);
 
 	public static readonly ServerClass ServerClass = new ServerClass("BaseEntity", DT_BaseEntity)
