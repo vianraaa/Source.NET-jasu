512665081cba707d86f79afe054fbc45d569e331
march <106459595+marchc1@users.noreply.github.com>
2025-09-20 12:46:42 -0700

    missing baseclass

--- Game.Client/HL2/C_BaseHLPlayer.cs
+++ Game.Client/HL2/C_BaseHLPlayer.cs
@@ -7,9 +7,9 @@ namespace Game.Client.HL2;
 
 public partial class C_BaseHLPlayer : C_BasePlayer
 {
-	public static readonly RecvTable DT_HL2_Player = [
+	public static readonly RecvTable DT_HL2_Player = new(DT_BasePlayer, [
 
-	];
+	]);
 	public static readonly new ClientClass ClientClass = new ClientClass("HL2_Player", null, null, DT_HL2_Player)
 															.WithManualClassID(StaticClassIndices.CHL2_Player);
 
