ded10c3e19dc22014451489ad5f4e459af68dfda
march <106459595+marchc1@users.noreply.github.com>
2025-09-22 14:01:49 -0700

    Create PlayerLocalData.cs

---
+++ Game.Server/PlayerLocalData.cs
@@ -0,0 +1,38 @@
+﻿using Source.Common;
+using Source.Common.Mathematics;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Server;
+
+public class PlayerLocalData
+{
+	// TODO: NETWORK VARS!!!!!
+	public bool Ducked;
+	public bool Ducking;
+	public bool InDuckJump;
+	public double DuckTime;
+	public double DuckJumpTime;
+	public double JumpTime;
+	public int StepSide;
+	public double FallVelocity;
+	public int OldButtons;
+	public int OldForwardMove;
+	public QAngle PunchAngle;
+	public QAngle PunchAngleVel;
+	public bool DrawViewmodel;
+	public bool WearingSuit;
+	public bool Poisoned;
+	public bool StepSize;
+	public bool AllowAutoMovement;
+	public bool SlowMovement;
+
+	//public static readonly SendTable DT_Local = new([
+
+	//]);
+	//public static readonly ServerClass ServerClass = new("Local", DT_Local);
+}
