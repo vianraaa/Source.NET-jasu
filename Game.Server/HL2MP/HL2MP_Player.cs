bf2ebae8b2e7992b612018e760874a3e29dedd7c
march <106459595+marchc1@users.noreply.github.com>
2025-09-26 13:27:28 -0700

    wtf is broken?

--- Game.Server/HL2MP/HL2MP_Player.cs
+++ Game.Server/HL2MP/HL2MP_Player.cs
@@ -12,17 +12,17 @@ using FIELD = FIELD<HL2MP_Player>;
 public class HL2MP_Player : HL2_Player
 {
 	public static readonly SendTable DT_HL2MPLocalPlayerExclusive = new([
-		SendPropVector(FIELD.OF(nameof(Origin)), -1, PropFlags.NoScale|PropFlags.ChangesOften, 0.0f, Constants.HIGH_DEFAULT),
+		SendPropVector(FIELD.OF(nameof(Origin)), 0, PropFlags.NoScale|PropFlags.ChangesOften, 0.0f, Constants.HIGH_DEFAULT),
 
-		SendPropFloat(FIELD.OF_VECTORELEM(nameof(EyeAngles), 0), 8, PropFlags.ChangesOften, -90.0f, 90.0f ),
-		SendPropAngle(FIELD.OF_VECTORELEM(nameof(EyeAngles), 1), 10, PropFlags.ChangesOften ),
+		SendPropFloat(FIELD.OF_VECTORELEM(nameof(EyeAngles), 0), 11, PropFlags.ChangesOften | PropFlags.RoundDown, 0, 360f ),
+		SendPropAngle(FIELD.OF_VECTORELEM(nameof(EyeAngles), 1), 11, PropFlags.ChangesOften | PropFlags.RoundDown, 0, 360f ),
 	]); public static readonly ServerClass SC_HL2MPLocalPlayerExclusive = new ServerClass("HL2MPLocalPlayerExclusive", DT_HL2MPLocalPlayerExclusive);
 
 	public static readonly SendTable DT_HL2MPNonLocalPlayerExclusive = new([
-		SendPropVector(FIELD.OF(nameof(Origin)), -1, PropFlags.CoordMPLowPrecision|PropFlags.ChangesOften, 0.0f, Constants.HIGH_DEFAULT),
+		SendPropVector(FIELD.OF(nameof(Origin)), 0, PropFlags.CoordMPLowPrecision|PropFlags.ChangesOften, 0.0f, Constants.HIGH_DEFAULT),
 
-		SendPropFloat(FIELD.OF_VECTORELEM(nameof(EyeAngles), 0), 8, PropFlags.ChangesOften, -90.0f, 90.0f),
-		SendPropAngle(FIELD.OF_VECTORELEM(nameof(EyeAngles), 1), 10, PropFlags.ChangesOften),
+		SendPropFloat(FIELD.OF_VECTORELEM(nameof(EyeAngles), 0), 11, PropFlags.ChangesOften | PropFlags.RoundDown, 0, 360f),
+		SendPropAngle(FIELD.OF_VECTORELEM(nameof(EyeAngles), 1), 11, PropFlags.ChangesOften | PropFlags.RoundDown, 0, 360f),
 
 	]); public static readonly ServerClass SC_HL2MPNonLocalPlayerExclusive = new ServerClass("HL2MPNonLocalPlayerExclusive", DT_HL2MPNonLocalPlayerExclusive);
 
