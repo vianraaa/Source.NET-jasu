2ca03d6ef03ac7229f16f6cf5e587de6e171a0d2
march <106459595+marchc1@users.noreply.github.com>
2025-09-28 10:42:07 -0700

    WIP gamerules

---
+++ Game.Shared/GameRules.cs
@@ -0,0 +1,56 @@
+﻿#if CLIENT_DLL
+global using GameRules = Game.Client.C_GameRules;
+global using GameRulesProxy = Game.Client.C_GameRulesProxy;
+namespace Game.Client;
+#else
+global using GameRules = Game.Server.GameRules;
+global using GameRulesProxy = Game.Server.GameRulesProxy;
+namespace Game.Server;
+#endif
+
+using Source.Common;
+using Source;
+
+using FIELD = Source.FIELD<GameRulesProxy>;
+using Game.Shared;
+
+public class
+#if CLIENT_DLL
+	C_GameRulesProxy
+#else
+	GameRulesProxy
+#endif
+	: SharedBaseEntity
+{
+	public virtual GameRules GameRules => gameRules!;
+	GameRules? gameRules = null;
+
+	public static GameRulesProxy? s_GameRulesProxy;
+
+	public static readonly
+	#if CLIENT_DLL
+		RecvTable
+#else
+		SendTable
+#endif
+		DT_GameRulesProxy = new([]);
+#if CLIENT_DLL
+	public static readonly new ClientClass ClientClass = new ClientClass("GameRulesProxy", null, null, DT_GameRulesProxy).WithManualClassID(StaticClassIndices.CGameRulesProxy);
+#else
+#pragma warning disable CS0109 // Member does not hide an inherited member; new keyword is not required
+	public static readonly new ServerClass ServerClass = new ServerClass("GameRulesProxy", DT_GameRulesProxy).WithManualClassID(StaticClassIndices.CGameRulesProxy);
+#pragma warning restore CS0109 // Member does not hide an inherited member; new keyword is not required
+#endif
+
+}
+
+public class
+#if CLIENT_DLL
+	C_GameRules
+#else
+	GameRules
+#endif
+// TODO: AutoGameSystemPerFrame
+{
+	public virtual ReadOnlySpan<char> Name() => "GameRules";
+}
---
+++ Game.Shared/GarrysMod/GMODGameRules.cs
@@ -0,0 +1,79 @@
+﻿#if CLIENT_DLL
+global using GMODGameRules = Game.Client.GarrysMod.C_GMODGameRules;
+global using GMODGameRulesProxy = Game.Client.GarrysMod.C_GMODGameRulesProxy;
+namespace Game.Client.GarrysMod;
+#else
+global using GMODGameRules = Game.Server.GarrysMod.GMODGameRules;
+global using GMODGameRulesProxy = Game.Server.GarrysMod.GMODGameRulesProxy;
+namespace Game.Server.GarrysMod;
+#endif
+
+using Source.Common;
+using Source;
+
+using FIELD = Source.FIELD<GMODGameRulesProxy>;
+using Game.Shared;
+
+public class
+#if CLIENT_DLL
+	C_GMODGameRulesProxy
+#else
+	GMODGameRulesProxy
+#endif
+	: GameRulesProxy
+{
+	public override GameRules GameRules => gmod_gamerules_data;
+	public GMODGameRules gmod_gamerules_data = new();
+	public static readonly
+#if CLIENT_DLL
+		RecvTable
+#else
+		SendTable
+#endif
+	DT_GMODRules = new(DT_GameRulesProxy, [
+#if CLIENT_DLL
+		RecvPropFloat(FIELD<GMODGameRules>.OF("TimeScale")),
+		RecvPropInt(FIELD<GMODGameRules>.OF("SkillLevel"))
+#else
+		SendPropFloat(FIELD<GMODGameRules>.OF("TimeScale"), 0, PropFlags.NoScale, 0, 0),
+		SendPropInt(FIELD<GMODGameRules>.OF("SkillLevel"), 32, 0)
+	]);
+#endif
+
+	public static readonly
+	#if CLIENT_DLL
+		RecvTable
+#else
+		SendTable
+#endif
+		DT_GMODGameRulesProxy = new(DT_GameRulesProxy, [
+#if CLIENT_DLL
+			RecvPropDataTable(nameof(gmod_gamerules_data), FIELD.OF(nameof(gmod_gamerules_data)), DT_GMODRules, DataTableRecvProxy_PointerDataTable)
+#else
+			SendPropDataTable(nameof(gmod_gamerules_data), DT_GMODRules)
+#endif
+		]);
+#if CLIENT_DLL
+	public static readonly new ClientClass ClientClass = new ClientClass("GMODGameRulesProxy", null, null, DT_GMODGameRulesProxy).WithManualClassID(StaticClassIndices.CGMODGameRulesProxy);
+#else
+#pragma warning disable CS0109 // Member does not hide an inherited member; new keyword is not required
+	public static readonly new ServerClass ServerClass = new ServerClass("GMODGameRulesProxy", DT_GMODGameRulesProxy).WithManualClassID(StaticClassIndices.CGMODGameRulesProxy);
+#pragma warning restore CS0109 // Member does not hide an inherited member; new keyword is not required
+#endif
+
+}
+
+public class
+#if CLIENT_DLL
+	C_GameRules
+#else
+	GMODGameRules
+#endif
+	: GameRules
+// TODO: AutoGameSystemPerFrame
+{
+	public override ReadOnlySpan<char> Name() => "GMODGameRules";
+
+	public float TimeScale;
+	public int SkillLevel;
+}
