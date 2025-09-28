60a478e8a4866e3c0ab7d04d2f2edfc40da4ccaf
march <106459595+marchc1@users.noreply.github.com>
2025-09-28 13:58:19 -0700

    Just do this instead for now

--- Game.Shared/BaseCombatWeaponShared.cs
+++ Game.Shared/BaseCombatWeaponShared.cs
@@ -5,8 +5,9 @@ global using BaseCombatWeapon = Game.Server.BaseCombatWeapon;
 #endif
 
 using Source.Common.Mathematics;
-
+using Source.Common;
 using System.Numerics;
+using Game.Shared;
 
 #if CLIENT_DLL
 namespace Game.Client;
@@ -14,13 +15,37 @@ namespace Game.Client;
 namespace Game.Server;
 #endif
 
+using Table =
+#if CLIENT_DLL
+    RecvTable;
+#else
+	SendTable;
+#endif
+
+using Class =
+#if CLIENT_DLL
+    ClientClass;
+#else
+	ServerClass;
+#endif
+
+
 public partial class
 #if CLIENT_DLL
-	C_BaseCombatWeapon
+    C_BaseCombatWeapon
 #else
 	BaseCombatWeapon
 #endif
 {
+	public static readonly Table DT_LocalWeaponData = new([
 
+	]); public static readonly Class SC_LocalWeaponData = new Class("LocalWeaponData", DT_LocalWeaponData);
+
+	public static readonly Table DT_LocalActiveWeaponData = new([
+
+	]); public static readonly Class SC_LocalActiveWeaponData = new Class("LocalActiveWeaponData", DT_LocalActiveWeaponData);
+
+	public static readonly Table DT_BaseCombatWeapon = new(DT_BaseAnimating, [
+
+	]); public static readonly new Class ServerClass = new Class("BaseCombatWeapon", DT_BaseCombatWeapon).WithManualClassID(StaticClassIndices.CBaseCombatWeapon);
 }
-
--- Source.Common/ClientClass.cs
+++ Source.Common/ClientClass.cs
@@ -38,51 +38,68 @@ public class ClientClass
 	public ClientClass? Next;
 	public int ClassID;
 
+	void AssembleDynamicCreateFn(Type t, [NotNull] ref CreateClientClassFn? createFn) {
+		if (t.IsAssignableTo(typeof(IClientEntity))) {
+			DynamicMethod method = new DynamicMethod($"CreateObjectDynImpl_{t.Name}", typeof(IClientNetworkable), [typeof(int), typeof(int)]);
+			ILGenerator il = method.GetILGenerator();
+
+			LocalBuilder ret = il.DeclareLocal(t);
+			LocalBuilder success = il.DeclareLocal(typeof(bool));
+
+			Label returnNull = il.DefineLabel();
+			Label returnObj = il.DefineLabel();
+
+			il.Emit(OpCodes.Newobj, t.GetConstructor(Type.EmptyTypes)!);
+			il.Emit(OpCodes.Stloc, ret);
+
+			il.Emit(OpCodes.Ldloc, ret);
+			il.Emit(OpCodes.Ldarg_0); // entNum
+			il.Emit(OpCodes.Ldarg_1); // serialNum
+			il.Emit(OpCodes.Callvirt, t.GetMethod("Init", new[] { typeof(int), typeof(int) })!);
+			il.Emit(OpCodes.Stloc, success);
+
+			il.Emit(OpCodes.Ldloc, success);
+			il.Emit(OpCodes.Brfalse_S, returnNull);
+
+			il.Emit(OpCodes.Ldloc, ret);
+			il.Emit(OpCodes.Br_S, returnObj);
+
+			il.MarkLabel(returnNull);
+			il.Emit(OpCodes.Ldnull);
+
+			il.MarkLabel(returnObj);
+			il.Emit(OpCodes.Ret);
+
+			createFn = method.CreateDelegate<CreateClientClassFn>();
+		}
+		else
+			createFn = (_, _) => throw new NotImplementedException("ClientClass did not define how to create this object");
+	}
+
+	public ClientClass(ReadOnlySpan<char> networkName, RecvTable recvTable, [CallerArgumentExpression(nameof(recvTable))] string? nameOfTable = null) {
+		Type t = WhoCalledMe(skipFrames: 2) ?? throw new NullReferenceException("This doesnt work as well as we hoped!");
+		AssembleDynamicCreateFn(t, ref CreateFn);
+
+		NetworkName = new(networkName);
+		RecvTable = recvTable;
+
+		if (nameOfTable != null)
+			recvTable.NetTableName = nameOfTable;
+
+		Next = Head;
+		Head = this;
+		ClassID = -1;
+	}
 	public ClientClass(ReadOnlySpan<char> networkName, CreateClientClassFn? createFn, CreateEventFn? createEventFn, RecvTable recvTable, [CallerArgumentExpression(nameof(recvTable))] string? nameOfTable = null) {
 		if (createFn == null) {
 			Type t = WhoCalledMe(skipFrames: 2) ?? throw new NullReferenceException("This doesnt work as well as we hoped!");
-			if (t.IsAssignableTo(typeof(IClientEntity))) {
-				DynamicMethod method = new DynamicMethod($"CreateObjectDynImpl_{t.Name}", typeof(IClientNetworkable), [typeof(int), typeof(int)]);
-				ILGenerator il = method.GetILGenerator();
-
-				LocalBuilder ret = il.DeclareLocal(t);
-				LocalBuilder success = il.DeclareLocal(typeof(bool));
-
-				Label returnNull = il.DefineLabel();
-				Label returnObj = il.DefineLabel();
-
-				il.Emit(OpCodes.Newobj, t.GetConstructor(Type.EmptyTypes)!);
-				il.Emit(OpCodes.Stloc, ret);
-
-				il.Emit(OpCodes.Ldloc, ret);
-				il.Emit(OpCodes.Ldarg_0); // entNum
-				il.Emit(OpCodes.Ldarg_1); // serialNum
-				il.Emit(OpCodes.Callvirt, t.GetMethod("Init", new[] { typeof(int), typeof(int) })!);
-				il.Emit(OpCodes.Stloc, success);
-
-				il.Emit(OpCodes.Ldloc, success);
-				il.Emit(OpCodes.Brfalse_S, returnNull);
-
-				il.Emit(OpCodes.Ldloc, ret);
-				il.Emit(OpCodes.Br_S, returnObj);
-
-				il.MarkLabel(returnNull);
-				il.Emit(OpCodes.Ldnull);
-
-				il.MarkLabel(returnObj);
-				il.Emit(OpCodes.Ret);
-
-				createFn = method.CreateDelegate<CreateClientClassFn>();
-			}
-			else
-				createFn = (_, _) => throw new NotImplementedException("ClientClass did not define how to create this object");
+			AssembleDynamicCreateFn(t, ref createFn);
 		}
 
 		CreateFn = createFn;
 		CreateEventFn = createEventFn;
 		NetworkName = new(networkName);
 		RecvTable = recvTable;
-
 
 		if (nameOfTable != null)
 			recvTable.NetTableName = nameOfTable;
