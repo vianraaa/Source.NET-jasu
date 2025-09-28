47850080662f7cba078f35c1afa313a7c5e201c0
march <106459595+marchc1@users.noreply.github.com>
2025-09-28 15:45:17 -0700

    point entities?

---
+++ Game.Shared/GarrysMod/SENT_Point.cs
@@ -0,0 +1,52 @@
+﻿#if CLIENT_DLL || GAME_DLL
+using Source.Common;
+using Game.Shared;
+using Source;
+
+#if CLIENT_DLL
+namespace Game.Client;
+using FIELD = Source.FIELD<C_SENT_Point>;
+#else
+namespace Game.Server;
+using FIELD = Source.FIELD<SENT_Point>;
+#endif
+
+using Table =
+#if CLIENT_DLL
+    RecvTable;
+#else
+	SendTable;
+#endif
+
+using Class =
+#if CLIENT_DLL
+    ClientClass;
+#else
+	ServerClass;
+#endif
+
+public partial class
+#if CLIENT_DLL
+    C_SENT_Point
+#else
+	SENT_Point
+#endif
+	: SharedBaseEntity
+{
+	public static readonly Table DT_SENT_Point = new(DT_BaseEntity, [
+#if CLIENT_DLL
+		RecvPropDataTable("ScriptedEntity", DT_ScriptedEntity)
+#elif GAME_DLL
+		SendPropDataTable("ScriptedEntity", DT_ScriptedEntity)
+#endif
+	]);
+
+	public static readonly new Class
+#if CLIENT_DLL
+		ClientClass
+#else
+		ServerClass
+#endif
+		= new Class("SENT_Point", DT_SENT_Point).WithManualClassID(StaticClassIndices.CSENT_point);
+}
+#endif
