8a5552b752849abcf38cc870bfd7157280eeb37b
march <106459595+marchc1@users.noreply.github.com>
2025-09-30 21:11:52 -0700

    Try adding more BSP rendering support

--- Source.Engine/MatSysInterface.cs
+++ Source.Engine/MatSysInterface.cs
@@ -5,9 +5,138 @@ using Microsoft.Extensions.DependencyInjection;
 using Source.Common;
 using Source.Common.Commands;
 using Source.Common.Formats.BSP;
+using Source.Common.GUI;
 using Source.Common.MaterialSystem;
 
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
 namespace Source.Engine;
+
+public struct MaterialList {
+	public short NextBlock;
+	public short Count;
+	public InlineArray15<BSPMSurface2> Surfaces;
+}
+
+public struct SurfaceSortGroup {
+	public short ListHead;
+	public short ListTail;
+	public ushort VertexCount;
+	public short GroupListIndex;
+	public ushort VertexCountNoDetail;
+	public ushort IndexCountNoDetail;
+	public ushort TriangleCount;
+	public ushort SurfaceCount;
+}	
+
+public class MSurfaceSortList {
+	const int MAX_MAT_SORT_GROUPS = 4;
+	public void Init(int maxSortIDs, int minMaterialLists) {
+
+	}
+
+	readonly int[] GroupOffset = new int[MAX_MAT_SORT_GROUPS];
+	readonly List<byte> GroupUsed = [];
+	readonly List<MaterialList> List = [];
+	readonly List<SurfaceSortGroup> Groups = [];
+	readonly List<SurfaceSortGroup>[] SortGroupLists = new List<SurfaceSortGroup>[MAX_MAT_SORT_GROUPS];
+
+	public void InitGroup(ref SurfaceSortGroup group) {
+		group.ListHead = -1;
+		group.ListTail = -1;
+		group.VertexCount = 0;
+		group.GroupListIndex = -1;
+		group.VertexCountNoDetail = 0;
+		group.IndexCountNoDetail = 0;
+		group.TriangleCount = 0;
+		group.SurfaceCount = 0;
+	}
+	public bool IsGroupUsed(int groupIndex) => (GroupUsed[(groupIndex >> 3)] & (1 << (groupIndex & 7))) != 0;
+	public void MarkGroupUsed(int groupIndex) => GroupUsed[groupIndex >> 3] |= checked((byte)(1 << (groupIndex & 7)));
+	public void MarkGroupNotUsed(int groupIndex) => GroupUsed[groupIndex >> 3] &= checked((byte)~(1 << (groupIndex & 7)));
+
+	internal void AddSurfaceToTail(ref BSPMSurface2 surface, int sortGroup, short sortID) {
+		Span<SurfaceSortGroup> groups = Groups.AsSpan();
+		int index = GroupOffset[sortGroup] + sortID;
+		ref SurfaceSortGroup group = ref groups[index];
+		if (!IsGroupUsed(index)) {
+			MarkGroupUsed(index);
+			InitGroup(ref group);
+		}
+		ref MaterialList list = ref Unsafe.NullRef<MaterialList>();
+		Span<MaterialList> m_list = List.AsSpan();
+		short prevIndex = -1;
+		int vertCount = ModelLoader.MSurf_VertCount(ref surface);
+		int triangleCount = vertCount - 2;
+		group.TriangleCount += (ushort)triangleCount;
+		group.SurfaceCount++;
+		group.VertexCount += (ushort)vertCount;
+		if ((ModelLoader.MSurf_Flags(ref surface) & SurfDraw.Node) != 0) {
+			group.VertexCountNoDetail += (ushort)vertCount;
+			group.IndexCountNoDetail += (ushort)(triangleCount * 3);
+		}
+		if (group.ListTail != -1) {
+			list = ref m_list[group.ListTail];
+			if (list.Count >= 15 /* list.Surfaces length */) {
+				prevIndex = group.ListTail;
+				list = ref Unsafe.NullRef<MaterialList>();
+			}
+		}
+		if (!Unsafe.IsNullRef(ref list)) {
+			list.Surfaces[list.Count] = surface;
+			list.Count++;
+		}
+		else {
+			List.Add(default);
+			short nextBlock = (short)(List.Count - 1);
+			// m_list may be invalid now! remake the span
+			m_list = List.AsSpan();
+
+			if (prevIndex >= 0) 
+				m_list[prevIndex].NextBlock = nextBlock;
+			
+			group.ListTail = nextBlock;
+			if (group.ListHead == -1) {
+				SortGroupLists[sortGroup].Add(group);
+				index = (short)(SortGroupLists[sortGroup].Count - 1);
+				group.GroupListIndex = (short)index;
+				group.ListHead = nextBlock;
+			}
+
+			list = ref m_list[nextBlock];
+			list.NextBlock = -1;
+			list.Count = 1;
+			list.Surfaces[0] = surface;
+		}
+	}
+
+	internal ref SurfaceSortGroup GetGroupForSortID(int sortGroup, int sortID) {
+		return ref GetGroupByIndex(GetIndexForSortID(sortGroup, sortID));
+	}
+
+	internal ref MaterialList GetSurfaceBlock(short index) {
+		return ref List.AsSpan()[index];
+	}
+
+	private int GetIndexForSortID(int sortGroup, int sortID) {
+		return GroupOffset[sortGroup] + sortID;
+	}
+
+	SurfaceSortGroup EmptyGroup;
+
+	private ref SurfaceSortGroup GetGroupByIndex(int groupIndex) {
+		if (!IsGroupUsed(groupIndex))
+			return ref EmptyGroup;
+		return ref Groups.AsSpan()[groupIndex];
+	}
+
+	internal ref BSPMSurface2 GetSurfaceAtHead(in SurfaceSortGroup group) {
+		if (group.ListHead == -1)
+			return ref Unsafe.NullRef<BSPMSurface2>();
+		return ref List.AsSpan()[group.ListHead].Surfaces[0];
+	}
+}
 
 public class MatSysInterface(IMaterialSystem materials, IServiceProvider services)
 {
@@ -61,12 +190,57 @@ public class MatSysInterface(IMaterialSystem materials, IServiceProvider service
 	readonly List<MeshList> Meshes = [];
 	readonly List<IMesh?> WorldStaticMeshes = [];
 	ConVar mat_max_worldmesh_vertices = new("65536", 0);
+	public static int VertexCountForSurfaceList(MSurfaceSortList list, in SurfaceSortGroup group) {
+		int vertexCount = 0;
+
+		for (short _blockIndex = group.ListHead; _blockIndex != -1; _blockIndex = list.GetSurfaceBlock(_blockIndex).NextBlock)  
+		{																			
+			ref MaterialList matList = ref list.GetSurfaceBlock(_blockIndex); 
+			for (int _index = 0; _index < matList.Count; ++_index)                
+			{																		
+				ref BSPMSurface2 surfID = ref matList.Surfaces[_index];
+				vertexCount += ModelLoader.MSurf_VertCount(ref surfID);
+			}
+		}
+
+		return vertexCount;
+	}
 	public void WorldStaticMeshCreate() {
 		FrameCount = 1;
 		WorldStaticMeshDestroy();
-		Assert(WorldStaticMeshes.Count == 0);
+		Meshes.Clear();
 
-		WorldStaticMeshes.Clear();
+		int sortIDs = 0;
+
+		Assert(WorldStaticMeshes.Count == 0);
+		WorldStaticMeshes.EnsureCountDefault(sortIDs);
+
+		MSurfaceSortList matSortArray = new();
+		matSortArray.Init(sortIDs, 512);
+		Span<int> sortIndex = stackalloc int[WorldStaticMeshes.Count];
+
+		for (int surfaceIndex = 0; surfaceIndex < host_state.WorldBrush.NumSurfaces; surfaceIndex++) {
+			ref BSPMSurface2 surfID = ref ModelLoader.SurfaceHandleFromIndex(surfaceIndex);
+			ModelLoader.MSurf_Flags(ref surfID) &= ~SurfDraw.TangentSpace;
+
+			if (ModelLoader.SurfaceHasDispInfo(ref surfID)) {
+				ModelLoader.MSurf_VertBufferIndex(ref surfID) = 0xFFFF;
+				continue;
+			}
+
+			matSortArray.AddSurfaceToTail(ref surfID, 0, ModelLoader.MSurf_MaterialSortID(ref surfID));
+		}
+
+		for (int i = 0; i < WorldStaticMeshes.Count; i++) {
+			ref readonly SurfaceSortGroup group = ref matSortArray.GetGroupForSortID(0, i);
+			int vertexCount = VertexCountForSurfaceList(matSortArray, group);
+
+			ref BSPMSurface2 surfID = ref matSortArray.GetSurfaceAtHead(in group);
+			WorldStaticMeshes[i] = null;
+			sortIndex[i] = !Unsafe.IsNullRef(ref surfID) ? FindOrAddMesh(ModelLoader.MSurf_TexInfo(ref surfID).Material, vertexCount) : -1;
+		}
+
+
 		using MatRenderContextPtr renderContext = new(materials);
 		var meshes = Meshes.AsSpan();
 		for (int i = 0; i < Meshes.Count; i++) {
@@ -76,6 +250,41 @@ public class MatSysInterface(IMaterialSystem materials, IServiceProvider service
 			meshBuilder.Begin(meshes[i].Mesh, MaterialPrimitiveType.Triangles, meshes[i].VertCount, 0);
 		}
 	}
+
+	private int FindOrAddMesh(IMaterial? material, int vertexCount) {
+		VertexFormat format = material.GetVertexFormat();
+
+		using MatRenderContextPtr renderContext = new(materials );
+
+		int nMaxVertices = renderContext.GetMaxVerticesToRender(material);
+		int worldLimit = mat_max_worldmesh_vertices.GetInt();
+		worldLimit = Math.Max(worldLimit, 1024);
+		if (nMaxVertices > worldLimit) {
+			nMaxVertices = mat_max_worldmesh_vertices.GetInt();
+		}
+
+		Span<MeshList> meshes = Meshes.AsSpan();
+
+		for (int i = 0; i < meshes.Length; i++) {
+			if (meshes[i].VertexFormat != format)
+				continue;
+
+			if (meshes[i].VertCount + vertexCount > nMaxVertices)
+				continue;
+
+			meshes[i].VertCount += vertexCount;
+			return i;
+		}
+
+		Meshes.Add(new() {
+			VertCount = vertexCount,
+			VertexFormat = format,
+			Material = material
+		});
+
+		return Meshes.Count - 1;
+	}
+
 	public void WorldStaticMeshDestroy() {
 
 	}
