e45c38f11e5fc5491a11421473a2be69854e4262
march <106459595+marchc1@users.noreply.github.com>
2025-10-06 18:52:06 -0700

    Crossbow and crowbar templates (still need DT's)

--- Game.Shared/GarrysMod/WeaponCrossbow.cs
+++ Game.Shared/GarrysMod/WeaponCrossbow.cs
@@ -1,4 +1,26 @@
 ﻿#if CLIENT_DLL || GAME_DLL
+using Source.Common;
 namespace Game.Shared.GarrysMod;
-public class WeaponCrossbow : BaseHL2MPCombatWeapon;
+using FIELD = Source.FIELD<WeaponCrossbow>;
+public class WeaponCrossbow : BaseHL2MPCombatWeapon
+{
+	public static readonly
+#if CLIENT_DLL
+		RecvTable
+#else
+		SendTable
+#endif
+		DT_WeaponCrossbow = new(DT_BaseHL2MPCombatWeapon, [
+#if CLIENT_DLL
+
+#else
+
+#endif
+		]);
+#if CLIENT_DLL
+	public static readonly new ClientClass ClientClass = new ClientClass("WeaponCrossbow", null, null, DT_WeaponCrossbow).WithManualClassID(StaticClassIndices.CWeaponCrossbow);
+#else
+	public static readonly new ServerClass ServerClass = new ServerClass("WeaponCrossbow", DT_WeaponCrossbow).WithManualClassID(StaticClassIndices.CWeaponCrossbow);
+#endif
+}
 #endif
--- Game.Shared/GarrysMod/WeaponCrowbar.cs
+++ Game.Shared/GarrysMod/WeaponCrowbar.cs
@@ -1,4 +1,26 @@
 ﻿#if CLIENT_DLL || GAME_DLL
+using Source.Common;
 namespace Game.Shared.GarrysMod;
-public class WeaponCrowbar : BaseHL2MPBludgeonWeapon;
+using FIELD = Source.FIELD<WeaponCrowbar>;
+public class WeaponCrowbar : BaseHL2MPBludgeonWeapon
+{
+	public static readonly
+#if CLIENT_DLL
+		RecvTable
+#else
+		SendTable
+#endif
+		DT_WeaponCrowbar = new(DT_BaseHL2MPBludgeonWeapon, [
+#if CLIENT_DLL
+
+#else
+
+#endif
+		]);
+#if CLIENT_DLL
+	public static readonly new ClientClass ClientClass = new ClientClass("WeaponCrowbar", null, null, DT_WeaponCrowbar).WithManualClassID(StaticClassIndices.CWeaponCrowbar);
+#else
+	public static readonly new ServerClass ServerClass = new ServerClass("WeaponCrowbar", DT_WeaponCrowbar).WithManualClassID(StaticClassIndices.CWeaponCrowbar);
+#endif
+}
 #endif
