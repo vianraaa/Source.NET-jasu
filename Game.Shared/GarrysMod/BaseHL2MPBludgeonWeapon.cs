0f7db55bfe039c5d59466293a60942f165ec691c
march <106459595+marchc1@users.noreply.github.com>
2025-10-06 18:40:26 -0700

    Setup templates for BludgeonWeapon, MachineGun

--- Game.Shared/GarrysMod/BaseHL2MPBludgeonWeapon.cs
+++ Game.Shared/GarrysMod/BaseHL2MPBludgeonWeapon.cs
@@ -1,4 +1,26 @@
 ﻿#if CLIENT_DLL || GAME_DLL
+using Source.Common;
 namespace Game.Shared.GarrysMod;
-public class BaseHL2MPBludgeonWeapon : BaseHL2MPCombatWeapon;
+using FIELD = Source.FIELD<BaseHL2MPBludgeonWeapon>;
+public class BaseHL2MPBludgeonWeapon : BaseHL2MPCombatWeapon
+{
+	public static readonly
+#if CLIENT_DLL
+		RecvTable
+#else
+		SendTable
 #endif
+		DT_BaseHL2MPBludgeonWeapon = new(DT_BaseHL2MPCombatWeapon, [
+#if CLIENT_DLL
+
+#else
+
+#endif
+		]);
+#if CLIENT_DLL
+	public static readonly new ClientClass ClientClass = new ClientClass("BaseHL2MPBludgeonWeapon", null, null, DT_BaseHL2MPBludgeonWeapon).WithManualClassID(StaticClassIndices.CBaseHL2MPBludgeonWeapon);
+#else
+	public static readonly new ServerClass ServerClass = new ServerClass("BaseHL2MPBludgeonWeapon", DT_BaseHL2MPBludgeonWeapon).WithManualClassID(StaticClassIndices.CBaseHL2MPBludgeonWeapon);
+#endif
+#endif
+}
--- Game.Shared/GarrysMod/HL2MPBludgeonWeapon.cs
+++
@@ -1,4 +0,0 @@
-﻿#if CLIENT_DLL || GAME_DLL
-namespace Game.Shared.GarrysMod;
-public class HL2MPBludgeonWeapon : BaseHL2MPCombatWeapon;
-#endif
